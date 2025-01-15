3a08cfaed09c7d9f1cc76d7d44d38ad4d2a47806
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-15 16:18:38 -0300

    Refactor noxfile.py and pyproject.toml: update Python version handling, enhance dev session logging, and remove unused dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,10 @@
-from typing import List
+import pathlib
 
 import nox
 from nox.sessions import Session
 
-python_version = "3.12"  # choose the python version you want to use
+PYTHON_VERSION = pathlib.Path(".python-version").read_text().strip()
+python_version = PYTHON_VERSION  # choose the python version you want to use
 python_matrix = ["3.10", "3.11", "3.12", "3.13"]
 python_matrix = python_version if python_version != "" else python_matrix
 backend = "uv"  # or "venv" or "conda"
@@ -33,15 +34,18 @@ def format(session: Session) -> None:
     session.run("ruff", "format", "--target-version=py312", ".")
 
 
-@nox.session(python=python_matrix, venv_backend=backend)
+@nox.session(python=python_matrix, venv_backend=backend, reuse_venv=True)
 def test(session: Session) -> None:
-    """Run the test suite."""
+    """Run the pytest test suite."""
     session.run("uv", "add", "--dev", "pytest", "pytest-cov", ".")
     session.run("pytest", "--cov", env={"COVERAGE_FILE": ".coverage"})
 
 
 # non-default sessions to run
-@nox.session(python=python_version, venv_backend=backend)
-def dev(session: Session, params: List[str]) -> None:
-    """Run the project using docker."""
-    session.run("docker-compose", "up", "--build")
+@nox.session(python=python_version, venv_backend=backend, reuse_venv=True)
+def dev(session: nox.Session) -> None:
+    """Run the project dev envirioment."""
+    name = session.posargs[0] if session.posargs else "Lautaro"
+    # check if docker client is installed
+    session.log(f">Hello {name}: docker is not installed")
+    session.log(f">Python version: {PYTHON_VERSION}")
