532349496ef9ac7a4e688b07440ba3e79f32dc21
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-16 17:42:18 -0300

    Update Python version and configure environment variables for GCP resources

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,10 +1,15 @@
 """A Google Cloud Python Pulumi program"""
 
+from os import getenv
+
 import pulumi
+from dotenv import load_dotenv
 from pulumi_gcp import storage
 
+load_dotenv()
+bucket_name = getenv("GCP_BUCKET_NAME", "my-bucket")
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket', location="US")
+bucket = storage.Bucket(bucket_name, location="US")
 
 # Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
+pulumi.export(bucket_name, bucket.url)
