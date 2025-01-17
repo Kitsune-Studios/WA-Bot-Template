a0fdc32ca4a25c4950fcae6306704ce23eca3a63
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-17 14:21:27 -0300

    Set default bucket name in GCP configuration for improved usability

--- infra/__main__.py
+++ infra/__main__.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from pulumi_gcp import storage
 
 load_dotenv()
-bucket_name = getenv("GCP_BUCKET")
+bucket_name = getenv("GCP_BUCKET", "kitsune-bucket")
 # Create a GCP resource (Storage Bucket)
 bucket = storage.Bucket(bucket_name, location="US")
 
