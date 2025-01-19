2e9fe856f55d02d3f6bc7a283c7c4c42b387aa8b
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-19 16:12:38 -0300

    Add create_fargate_service to AWS provider exports

--- infra/Providers/aws/__init__.py
+++ infra/Providers/aws/__init__.py
@@ -4,7 +4,12 @@ Here we demonstrate how to selectively import and re-export
 modules to create a cleaner API for the user.
 """
 
+from .aws_fargate import create_fargate_service
 from .ec2 import create_ec2_instance  # Import specific functions/classes
 from .s3 import create_s3_bucket
 
-__all__ = ["create_ec2_instance", "create_s3_bucket"]  # Explicitly list what to expose
+__all__ = [
+    "create_ec2_instance",
+    "create_fargate_service",
+    "create_s3_bucket",
+]  # Re-export the modules
