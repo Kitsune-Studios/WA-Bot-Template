76ecb9d1de781bdb4ae34f0eafaf2cd18ceda7e5
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-19 01:50:14 -0300

    Remove Cloud Run service creation from main module and retain only storage bucket setup

--- infra/__main__.py
+++ infra/__main__.py
@@ -5,31 +5,13 @@ from os import getenv
 import pulumi
 from dotenv import load_dotenv
 from pulumi import Config
-from pulumi_gcp import cloudrun, storage
+from pulumi_gcp import storage
 
 config = Config()
 load_dotenv()
 bucket_name = getenv("GCP_BUCKET", "kitsune-bucket")
 # Create a GCP resource (Storage Bucket)
 bucket = storage.Bucket(bucket_name, location="US")
-
-# Create a GCP resource (Cloud Run Service) using the Dockerfile
-cloud_run = cloudrun.Service(
-    "kitsune-serverless",
-    location=Config("gcp").require("region"),
-    template=cloudrun.ServiceTemplateArgs(
-        spec=cloudrun.ServiceTemplateSpecArgs(
-            containers=[
-                cloudrun.ServiceTemplateSpecContainerArgs(
-                    image="gcr.io/cloudrun/hello",
-                    resources=cloudrun.ServiceTemplateSpecContainerResourcesArgs(
-                        limits={"cpu": "1", "memory": "512Mb"},
-                    ),
-                ),
-            ],
-        ),
-    ),
-)
 
 # Export the DNS name of the bucket
 pulumi.export(bucket_name, bucket.url)
