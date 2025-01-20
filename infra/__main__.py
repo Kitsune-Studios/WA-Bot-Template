f04ac71d4d049a77d0eeea97d0815229ce331802
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-20 13:21:17 -0300

    Add Visual Studio Code to .gitignore and update GCP resource location in main.py

--- infra/__main__.py
+++ infra/__main__.py
@@ -9,15 +9,25 @@ from pulumi_gcp import compute, storage
 load_dotenv()
 GCP = "gcp_"  # Prefix for GCP resources
 AWS = "aws_"  # Prefix for AWS resources
+LOCATION = getenv("GCP_LOCATION", "us-east1")
 
-LOCATION = getenv("GCP_LOCATION", "us-central1")
 BUCKET_NAME = getenv("BUCKET_NAME", "kitsune-bucket")
 
-gcp_bucket = storage.Bucket(GCP + BUCKET_NAME, location=LOCATION)
+gcp_bucket = storage.Bucket(GCP + BUCKET_NAME, location="US")
 gcp_compute = compute.Instance(
     GCP + "instance",
-    machine_type="n1-standard-1",
-    zone="us-central1-a",
+    machine_type="e2-micro",
+    zone=LOCATION,
+    boot_disk=compute.InstanceBootDiskArgs(
+        initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
+            image="debian-cloud/debian-9",
+        ),
+    ),
+    network_interfaces=[
+        compute.InstanceNetworkInterfaceArgs(
+            network=compute.NetworkArgs(name="default"),
+        ),
+    ],
 )
 
 # Create a GCP resource (Storage Bucket)
