7f4903b261c2da7ac15495a6256dd8c48536e183
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-20 11:49:48 -0300

    Refactor cloud resource creation to support both AWS S3 and GCP Storage buckets

--- infra/__main__.py
+++ infra/__main__.py
@@ -5,19 +5,23 @@ from os import getenv
 import pulumi
 from dotenv import load_dotenv
 from pulumi import Config
+from pulumi_aws import s3 as aws_s3
+from pulumi_gcp import storage as gcp_storage
 
 config = Config()
 load_dotenv()
-
+bucket_name = getenv("GCP_BUCKET", "kitsune-bucket")
 # chosse between AWS and GCP
 if config.get("cloud_provider") == "aws":
     # Create an AWS resource (S3 Bucket)
-    bucket = pulumi.Aws.S3.Bucket("kitsune-bucket")
+    bucket = aws_s3.Bucket(bucket_name)
     # Export the DNS name of the bucket
+else:
+    # Create a GCP resource (Storage Bucket)
+    bucket = gcp_storage.Bucket(bucket_name)
 
 
-bucket_name = getenv("GCP_BUCKET", "kitsune-bucket")
 # Create a GCP resource (Storage Bucket)
 
 # Export the DNS name of the bucket
-pulumi.export(bucket_name, bucket.url)
+pulumi.export(bucket_name, bucket_name.url)
