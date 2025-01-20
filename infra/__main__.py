2621f54dc77b38bcebfe9300fbb61ca98304aeeb
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-01-20 11:58:43 -0300

    Refactor CI workflow and update GCP bucket handling to use environment variables

--- infra/__main__.py
+++ infra/__main__.py
@@ -5,23 +5,16 @@ from os import getenv
 import pulumi
 from dotenv import load_dotenv
 from pulumi import Config
-from pulumi_aws import s3 as aws_s3
 from pulumi_gcp import storage as gcp_storage
 
 config = Config()
 load_dotenv()
-bucket_name = getenv("GCP_BUCKET", "kitsune-bucket")
+LOCATION = getenv("GCP_LOCATION", "us-central1")
+BUCKET_NAME = getenv("GCP_BUCKET", "kitsune-bucket")
+bucket = gcp_storage.Bucket(BUCKET_NAME, location=LOCATION)
 # chosse between AWS and GCP
-if config.get("cloud_provider") == "aws":
-    # Create an AWS resource (S3 Bucket)
-    bucket = aws_s3.Bucket(bucket_name)
-    # Export the DNS name of the bucket
-else:
-    # Create a GCP resource (Storage Bucket)
-    bucket = gcp_storage.Bucket(bucket_name)
-
 
 # Create a GCP resource (Storage Bucket)
 
 # Export the DNS name of the bucket
-pulumi.export(bucket_name, bucket.url)
+pulumi.export(BUCKET_NAME, bucket.url)
